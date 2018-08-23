7b669c48e0d0183588b83722796659d204354698
Dan Sensecall <dan@sensecall.co.uk>
2018-08-23 12:06:34 +0100

    Estimate journey

---
+++ app/views/version-2/routes.js
@@ -0,0 +1,25 @@
+const express = require('express')
+
+const router = new express.Router()
+
+router.get('/', (req, res) => {
+	res.redirect(`/${req.version}/start`)
+})
+
+router.get('/login', (req, res) => {
+	var currentPage = 'login'
+	res.render(`${req.version}/login`,{currentPage})
+})
+
+router.get('/estimator/account-check', (req, res) => {
+	var hasAccount = req.query.hasAccount
+	if (hasAccount === 'yes') {
+		res.redirect(`/${req.version}/login?redirect=estimator/estimate`)
+	} else if (hasAccount === 'no') {
+		res.redirect(`estimate`)
+	} else {
+		res.render(`${req.version}/estimator/account-check`)
+	}
+})
+
+module.exports = router
