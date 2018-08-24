01bce56a84baeb8dc7502e5de24cb7bfd807c204
Dan Sensecall <dan@sensecall.co.uk>
2018-08-24 08:04:30 +0100

    New estimator flow

--- app/views/version-2/routes.js
+++ app/views/version-2/routes.js
@@ -16,9 +16,20 @@ router.get('/estimator/account-check', (req, res) => {
 	if (hasAccount === 'yes') {
 		res.redirect(`/${req.version}/login?redirect=funding-projection`)
 	} else if (hasAccount === 'no') {
-		res.redirect(`add-apprenticeship`)
+		res.redirect(`business-details`)
 	} else {
 		res.render(`${req.version}/estimator/account-check`)
+	}
+})
+
+router.get('/estimator/apprenticeships-list', (req, res) => {
+	var addMoreApprenticeships = req.query.addMoreApprenticeships
+	if (addMoreApprenticeships === 'yes') {
+		res.redirect(`/${req.version}/estimator/add-apprenticeship`)
+	} else if (addMoreApprenticeships === 'no') {
+		res.redirect(`/${req.version}/funding-projection`)
+	} else {
+		res.render(`${req.version}/estimator/apprenticeships-list`)
 	}
 })
 
